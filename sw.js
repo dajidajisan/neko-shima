b355acac9402e798691f4b4d4810051f2bb5bcb8
dajidajisan <dtomomatsu@gmail.com>
2026-08-16 08:42:04 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Caches the core app files so it can still open (and be played) without
    an internet connection after it has been opened at least once. */
 
-const CACHE_NAME = 'neko-shima-v3';
+const CACHE_NAME = 'neko-shima-v5';
 const CORE_ASSETS = [
   './',
   './index.html',
